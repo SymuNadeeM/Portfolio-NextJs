fd025d76b4262bdab4276272effa4e53338d1cec
SymuNadeem <msymunnadeem@gmail.com>
2024-04-09 22:12:25 +0600

    sidebar dynamic

--- src/app/data/profileData.js
+++ src/app/data/profileData.js
@@ -10,20 +10,42 @@ import {
 import { FaLocationDot } from "react-icons/fa6";
 import userPic from "../assets/Symun2.jpg";
 
-export const profileData = [{
-  name: "Md. Symun Majumdar",
-  occupation: "Front-End Web Developer",
-  imageUrl: userPic,
-  socialMedia: [
-    { icon: FaFacebookF, link: "#",color: "#1773ea" },
-    { icon: FaTwitter, link: "#",color: "#1773ea" },
-    { icon: FaInstagram, link: "#", color: "#1773ea" },
-    { icon: FaLinkedinIn, link: "#", color: "#1773ea" },
-  ],
-  contactInfo: [
-    { icon: FaMobileAlt, label: "Phone", value: "+123 456 7890" },
-    { icon: FaEnvelopeOpenText, label: "Email", value: "msymunnadeem@gmail.com" },
-    { icon: FaLocationDot, label: "Location", value: "Mohammadpur Dhaka Bangladesh" },
-    { icon: FaCalendarAlt, label: "Birthday", value: "June 12, 1998" },
-  ],
-}] 
+export const profileData = [
+  {
+    name: "Md. Symun Majumdar",
+    occupation: "Front-End Web Developer",
+    imageUrl: userPic,
+    socialMedia: [
+      { icon: FaFacebookF, link: "#", color: "#1773ea" },
+      { icon: FaTwitter, link: "#", color: "#1c9cea" },
+      { icon: FaInstagram, link: "#", color: "#e12a72" },
+      { icon: FaLinkedinIn, link: "#", color: "#144679" },
+    ],
+    contactInfo: [
+      {
+        icon: FaMobileAlt,
+        label: "Phone",
+        value: "+123 456 7890",
+        color: "#E93B81",
+      },
+      {
+        icon: FaEnvelopeOpenText,
+        label: "Email",
+        value: "msymunnadeem@gmail.com",
+        color: "#6AB5B9",
+      },
+      {
+        icon: FaLocationDot,
+        label: "Location",
+        value: "Mohammadpur Dhaka Bangladesh",
+        color: "#FD7590",
+      },
+      {
+        icon: FaCalendarAlt,
+        label: "Birthday",
+        value: "June 12, 1998",
+        color: "#C17CEB",
+      },
+    ],
+  },
+];
