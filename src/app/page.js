243849e347faaeb6c727df615f3c877c055054dd
SymuNadeem <msymunnadeem@gmail.com>
2024-04-10 16:25:31 +0600

    home page about

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+import { FaLaptopCode } from "react-icons/fa";
+import { SiYourtraveldottv } from "react-icons/si";
+
 export default function Home() {
   return (
     <section>
@@ -6,7 +9,7 @@ export default function Home() {
           <h2 className="text-3.5 capitalize inline-block text-white  font-bold">
             About
           </h2>
-          <div className="w-[200px] h-[3px] bg-gradient-to-r from-[#fa5252] to-[#dd2476]"></div>
+          <div className="w-[200px] h-[3px] rounded-md bg-gradient-to-r from-[#fa5252] to-[#dd2476]"></div>
         </div>
 
         <p className="max-w-[632px] leading-7">
@@ -25,6 +28,50 @@ export default function Home() {
           to tackle new challenges and push the boundaries of what's possible on
           the web.
         </p>
+        {/* achievement card */}
+        <div className="mt-10 grid grid-cols-3 gap-3">
+          <div className="bg-grayBackground rounded-md  p-4 flex flex-col justify-center items-center">
+            <h2 className="text-3xl font-semibold"> 3+ </h2>
+            <p className="pt-2 text-sm uppercase text-center">
+              Years of Experience
+            </p>
+          </div>
+          <div className="bg-grayBackground rounded-md  p-4 flex flex-col justify-center items-center">
+            <h2 className="text-3xl font-semibold"> 30+ </h2>
+            <p className="pt-2 text-sm uppercase text-center">
+              PROJECTS COMPLETED
+            </p>
+          </div>
+          <div className="bg-grayBackground rounded-md  p-4 flex flex-col justify-center items-center">
+            <h2 className="text-3xl font-semibold"> 10+ </h2>
+            <p className="pt-2 text-sm uppercase text-center"> HAPPY CLIENTS</p>
+          </div>
+        </div>
+        <div className="mt-10">
+          <div className="flex items-center gap-4 mb-7.5">
+            <h3 className="text-2xl text-white font-medium">My Interests</h3>
+            <div className="w-[80px] h-[1.8px] rounded-md bg-gradient-to-r from-[#fa5252] to-[#dd2476]"></div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center justify-center gap-2">
+              <SiYourtraveldottv size={20} className="text-white" />
+              <p className="font-medium">Travel</p>
+            </div>
+            <div className="flex items-center justify-center gap-2">
+              <FaLaptopCode size={20} className="text-white" />
+              <p className="font-medium">Coding</p>
+            </div>
+
+            <div className=" flex items-center justify-center gap-2">
+              <i class="fa-solid fa-drumstick-bite text-[19px] dark:text-[#fff]  text-btnbg"></i>
+              <p className=" font-Robbi dark:text-dtext1">Eating</p>
+            </div>
+            <div className=" flex items-center justify-center gap-2">
+              <i class="fa-solid fa-baseball-bat-ball text-[19px] dark:text-[#fff]  text-btnbg"></i>
+              <p className=" font-Robbi dark:text-dtext1">Playing</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
