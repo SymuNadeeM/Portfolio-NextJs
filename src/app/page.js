0962baa428368732f95b44b7e2f0776dc32ce3fb
SymuNadeem <msymunnadeem@gmail.com>
2024-04-10 15:18:47 +0600

    home page start

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,31 @@
-
-
 export default function Home() {
   return (
-    <div className=" text-red-700 text-4xl">
-        hello bd
-        <p className="text-2xl font-Roboto text-orange-500">hello bd</p>
-        <h2 className="text-2xl font-Poppins text-orange-500">hello bd</h2>
-    </div>
+    <section>
+      <div className="mt-10 p-13 bg-black rounded-[20px]">
+        <div className="flex items-center gap-4 mb-3.5">
+          <h2 className="text-3.5 capitalize inline-block text-white  font-bold">
+            About
+          </h2>
+          <div className="w-[200px] h-[3px] bg-gradient-to-r from-[#fa5252] to-[#dd2476]"></div>
+        </div>
+
+        <p className="max-w-[632px] leading-7">
+          I am a dedicated frontend web developer with a passion for crafting
+          engaging and dynamic user experiences. With expertise in JavaScript,
+          React, Next.js, Node.js, CSS, Bootstrap, and Tailwind CSS, I thrive on
+          bringing designs to life with seamless functionality and aesthetic
+          appeal.
+        </p>
+        <p className="max-w-[632px] leading-7 mt-3">
+          Having honed my skills at Nurit Software Company, I've gained
+          invaluable experience collaborating within team environments,
+          communicating effectively, and delivering high-quality solutions under
+          tight deadlines. My commitment to staying abreast of the latest trends
+          and technologies in web development ensures that I'm always equipped
+          to tackle new challenges and push the boundaries of what's possible on
+          the web.
+        </p>
+      </div>
+    </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors:{
-       grayBackground: "#1d1d1d",
-       grayText: "#a6a6a6",
-       fbColor:"#1773ea",
-       twitter: "#1c9cea",
-       instr:"#e12a72",
-       linkEnd: "#144679",
-       mobile: "#E93B81",
-       borderColor: "#333",
-       emailColor: "#6AB5B9",
-       locationColor: "#FD7590",
-       calendarColor:"#C17CEB"
+      colors: {
+        grayBackground: "#1d1d1d",
+        grayText: "#a6a6a6",
+        fbColor: "#1773ea",
+        twitter: "#1c9cea",
+        instr: "#e12a72",
+        linkEnd: "#144679",
+        mobile: "#E93B81",
+        borderColor: "#333",
+        emailColor: "#6AB5B9",
+        locationColor: "#FD7590",
+        calendarColor: "#C17CEB",
       },
       fontFamily: {
         Roboto: "'Roboto Slab', 'serif'",
@@ -28,14 +28,19 @@ module.exports = {
         2.5: "10px",
         3.5: "15px",
         7.5: "30px",
-        25: "100px"
+        13: "50px",
+        25: "100px",
       },
       margin: {
         2.5: "10px",
         3.5: "15px",
         7.5: "30px",
-        25: "100px"
+        13: "50px",
+        25: "100px",
       },
+      fontSize:{
+        3.5 : "36px"
+      }
     },
   },
   plugins: [],
