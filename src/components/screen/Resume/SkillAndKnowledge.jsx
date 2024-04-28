e5864f494f860cceced69292720f4ba3498583ec
SymuNadeem <msymunnadeem@gmail.com>
2024-04-28 16:42:35 +0600

    skill

--- src/components/screen/Resume/SkillAndKnowledge.jsx
+++ src/components/screen/Resume/SkillAndKnowledge.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import SubTitle from "@/components/shared/SubTitle";
-// import { SiHyperskill } from "react-icons/si";
+import skillsData from "@/data/skillData";
 import { Line } from "rc-progress";
 import { GiSkills } from "react-icons/gi";
 
@@ -9,36 +9,25 @@ const SkillAndKnowledge = () => {
   return (
     <div className="mt-10">
       <div className="flex gap-2">
-        {/* <SiHyperskill size={32} className="text-[#dd2476]" /> */}
         <GiSkills size={32} className="text-[#dd2476]" />
         <SubTitle subtitle="Working Skills" />
       </div>
       <div className="grid grid-cols-2 gap-10">
         <div className="space-y-2">
-          <div>
-            <div className="flex items-center justify-between">
-              <h4>css</h4>
-              <h5>80%</h5>
+          {skillsData.map((skill, index) => (
+            <div key={index}>
+              <div className="flex items-center justify-between">
+                <h4>{skill.name}</h4>
+                <h5>{skill.percent}%</h5>
+              </div>
+              <Line
+                percent={skill.percent}
+                strokeWidth={1}
+                trailWidth={1}
+                strokeColor="#41B06E"
+              />
             </div>
-            <Line
-              percent={80}
-              strokeWidth={1}
-              trailWidth={1}
-              strokeColor="#41B06E"
-            />
-          </div>
-          <div>
-            <div className="flex items-center justify-between">
-              <h4>html</h4>
-              <h5>90%</h5>
-            </div>
-            <Line
-              percent={90}
-              strokeWidth={1}
-              trailWidth={1}
-              strokeColor="#41B06E"
-            />
-          </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/data/skillData.js
@@ -0,0 +1,7 @@
+const skillsData = [
+  { name: "css", percent: 80 },
+  { name: "html", percent: 90 },
+  { name: "html2", percent: 80 }
+];
+
+export default skillsData;
