ebb90dde94a03f51e2d5830f5bada2c896ca19ea
SymuNadeem <msymunnadeem@gmail.com>
2024-04-28 11:09:09 +0600

    experience

--- src/components/screen/Resume/Experience.jsx
+++ src/components/screen/Resume/Experience.jsx
@@ -1,17 +1,27 @@
 import SubTitle from "@/components/shared/SubTitle";
+import { experience } from "@/data/experience";
 import { MdOutlineWorkOutline } from "react-icons/md";
 
 const Experience = () => {
   return (
     <div>
-      <div className="flex  gap-2">
+      <div className="flex gap-2 mt-10">
         <MdOutlineWorkOutline size={32} className="text-[#dd2476]" />
         <SubTitle subtitle="Experience" />
       </div>
-      <div className="grid grid-cols-2 gap-7">
-          <div className="px-5 pb-5 pt-6 border border-borderRe">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero nobis sit voluptas voluptate dolor eum veritatis facilis corrupti blanditiis sunt illum est quasi nemo animi quis, cumque quia iusto quo.
+      <div className="grid md:grid-cols-2 gap-9">
+        {experience.map((experience, index) => (
+          <div
+            key={index}
+            className="px-5 pb-5 pt-6 border border-borderRe rounded-[12px] space-y-2"
+          >
+            <span>{experience.date}</span>
+            <h4 className="text-lg text-white font-medium leading-6">
+              {experience.title}
+            </h4>
+            <p>{experience.company}</p>
           </div>
+        ))}
       </div>
     </div>
   );
