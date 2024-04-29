1b987fcd64e7b327475262ae3b975192f6161746
SymuNadeem <msymunnadeem@gmail.com>
2024-04-29 23:10:43 +0600

    knowledge

--- src/app/works/page.jsx
+++ src/app/works/page.jsx
@@ -1,9 +1,15 @@
+import TitleText from "@/components/shared/TitleText";
 
 const WorkPage = () => {
   return (
-    <div>
-      WorkPage
-    </div>
+    <section>
+      <div className="mt-10 p-13 bg-black rounded-[20px]">
+        <TitleText title="Portfolio" />
+         <div>
+           g
+         </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/screen/Resume/Knowledge.jsx
+++ src/components/screen/Resume/Knowledge.jsx
@@ -1,4 +1,5 @@
 import SubTitle from "@/components/shared/SubTitle";
+import { knowledgeItems } from "@/data/knowledgeItems";
 import { GiGiftOfKnowledge } from "react-icons/gi";
 
 const Knowledge = () => {
@@ -9,18 +10,11 @@ const Knowledge = () => {
         <SubTitle subtitle="Knowledges" />
       </div>
       <div className="flex flex-wrap gap-x-3 gap-y-5">
-        <div className="bg-knowColor py-[5px] px-5 rounded-lg">Node.js</div>
-        <div className="bg-knowColor py-[5px] px-5 rounded-lg">MongoDB</div>
-        <div className="bg-knowColor py-[5px] px-5 rounded-lg">Express.js</div>
-        <div className="bg-knowColor py-[5px] px-5 rounded-lg">Redux</div>
-        <div className="bg-knowColor py-[5px] px-5 rounded-lg">GitHub</div>
-        <div className="bg-knowColor py-[5px] px-5 rounded-lg">
-          Communication
-        </div>
-        <div className="bg-knowColor py-[5px] px-5 rounded-lg">
-          Time Management
-        </div>
-        <div className="bg-knowColor py-[5px] px-5 rounded-lg">Flexibility</div>
+        {knowledgeItems.map((item, index) => (
+          <div key={index} className="bg-knowColor py-[5px] px-5 rounded-lg">
+            {item.name}
+          </div>
+        ))}
       </div>
     </div>
   );
