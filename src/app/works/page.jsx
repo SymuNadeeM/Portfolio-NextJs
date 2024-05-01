69e6d7e1b55d6940b7537baf47fc819adedad44c
SymuNadeem <msymunnadeem@gmail.com>
2024-05-01 09:07:04 +0600

    work page

--- src/app/works/page.jsx
+++ src/app/works/page.jsx
@@ -1,13 +1,30 @@
+"use client";
 import TitleText from "@/components/shared/TitleText";
+import { tabsData } from "@/data/tabsData";
+import { useState } from "react";
 
 const WorkPage = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <section>
       <div className="mt-10 p-13 bg-black rounded-[20px]">
         <TitleText title="Portfolio" />
-         <div>
-           g
-         </div>
+        <div className="flex">
+          {tabsData.map((tab, index) => (
+            <button
+              key={index}
+              className={`mr-4 px-4 py-2 rounded ${
+                activeTab === index ? "bg-blue-500 text-white" : "bg-gray-200"
+              }`}
+              onClick={() => setActiveTab(index)}
+            >
+              {tab.title}
+            </button>
+          ))}
+        </div>
+
+        <div>{tabsData[activeTab].content}</div>
       </div>
     </section>
   );
---
+++ src/data/tabsData.js
@@ -0,0 +1,16 @@
+export const tabsData = [
+  {
+    
+    title: "Tab 1",
+    content: <div>Content of Tab 1</div>,
+  },
+  { 
+    title: "Tab 2",
+    content: <div>Content of Tab 2</div>,
+  },
+  {
+    
+    title: "Tab 3",
+    content: <div>Content of Tab 3</div>,
+  },
+];
